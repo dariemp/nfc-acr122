e0969334a417b2b18d7470f1b9076cc7b88f9443
Dariem Pérez Herrera <dariemp@gmail.com>
2016-08-30 02:09:51 -0500

    Initial commit

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,11 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.kikiscene.nfc.reader.acr122u.plugin": "0.0.1",
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-nfc-acr122u": "0.0.1"
+};
+// BOTTOM OF METADATA
+});
